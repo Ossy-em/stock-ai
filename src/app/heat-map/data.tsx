fab0eff93c8dc406dec468cfe93b1533543b16db
Ossy-em <emosinachi@gmail.com>
2026-03-03 22:10:13 +0100

    initial commit

---
+++ src/app/heat-map/data.tsx
@@ -0,0 +1,26 @@
+
+const dataMap = [
+    {
+        Technology:
+            ["Apple", "Microsoft", "NVIDIA", "Adobe", "Salesforce", "Oracle", "Cisco", "Intel", "AMD", "ServiceNow", "IBM", "Broadcom", "Shopify", "Snowflake", "Palantir",]
+    },
+    {
+        Financials:
+            ["JPMorgan Chase", "Bank of America", "Wells Fargo", "Citigroup", "Goldman Sachs", "Morgan Stanley", "American Express", "Visa", "Mastercard", "PayPal",]
+    },
+    {
+        Healthcare: [
+            "Johnson & Johnson", "Pfizer", "Merck", "AbbVie", "Eli Lilly", "Amgen", "UnitedHealth Group", "Moderna",]
+    },
+    {
+        Fintech: [
+            "SoFi",
+            "Block (Square)",
+            "PayPal",
+            "Robinhood",
+            "Affirm",
+            "Wise",
+            "Coinbase"
+        ]
+    }]
+    export default dataMap;
